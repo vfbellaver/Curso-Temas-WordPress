a400f2a924945c4b1baae3aed50f2abf778b5471
vfbellaver <vfbellaver@gmail.com>
2018-01-09 23:07:23 -0300

    Criado arquivo index e style.css, screenshot do tema

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Curso Temas </title>
+</head>
+<body>
+<p>Este é um curso sobre customização de temas WordPress</p>
+</body>
+</html>
